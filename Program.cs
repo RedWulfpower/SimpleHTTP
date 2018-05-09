2d522c51d67d907fcbdeaf0a1797044cfa2e93f8
Florian Claus <florian.claus96@web.de>
2018-05-09 13:40:06 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHTTP
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SimpleHTTPServer myServer = null;
+      if (args.Length == 2)
+      {
+        myServer = new SimpleHTTPServer(args[0], Convert.ToInt32(args[1]));
+      }
+      
+
+      Console.WriteLine("Server is running on this port: " + myServer.Port.ToString());
+      Console.WriteLine("Server Root: " + myServer.RootDirectory);
+
+
+      while (true)
+      {
+
+      }
+      //Console.WriteLine("Server is stoping ");
+
+    }
+  }
+}
